fbfd96b37153a16959967000deb4af712df748b6
tiwiz <roberto.orgiu@gmail.com>
2014-01-20 20:46:25 +0100

    Initial commit

---
+++ src/it/tiwiz/TestGoogleNow/MyActivity.java
@@ -0,0 +1,15 @@
+package it.tiwiz.TestGoogleNow;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
